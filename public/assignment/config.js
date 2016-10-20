6d6ec1d2fed7268656ad404487266163922d537e
Vineeth Ravindra <Vineeth@ViNe2Th.local>
2016-10-20 15:13:03 -0400

    updating config

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -62,16 +62,16 @@
                 controller:"WidgetListController",
                 controllerAs:"model"
             })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/widget/widget-chooser.view.client.html", 
+                controller: "NewWidgetController", 
+                controllerAs:"model"
+            })
             .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
                 templateUrl: "views/widget/widget-edit.view.client.html", 
                 controller: "EditWidgetController", 
                 controllerAs:"model"
             })   
-            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
-                templateUrl: "views/widget/widget-chooser.view.client.html", 
-                controller: "NewWidgetController", 
-                controllerAs: "model"
-            })
             .otherwise({
                 redirectTo: "/",
                 controller: "LoginController",
